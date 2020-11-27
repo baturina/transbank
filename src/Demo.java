881398c6e997c7ae4e946b7b60d0b426467e37e9
Anastasia Baturina <89099934219@mail.ru>
2020-11-28 00:21:54 +0300

    initial commit

---
+++ src/Demo.java
@@ -0,0 +1,10 @@
+public class Demo {
+    public static void main(String[] args) {
+        int balance = 2_000_000_000;
+        int transfer = 500_000_000;
+        int resultbalance = balance + transfer;
+        System.out.println(resultbalance);
+
+    }
+}
+
